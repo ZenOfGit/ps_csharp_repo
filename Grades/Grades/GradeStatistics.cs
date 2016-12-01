a29278e04160612ebeb8176306f9d8ebba2f02be
Brad Miller <bmiller@healthgrades.com>
2016-12-01 15:33:37 -0600

    first commit

---
+++ Grades/Grades/GradeStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Grades
+{
+	public class GradeStatistics
+	{
+
+		public GradeStatistics()
+		{
+			HighestGrade = 0;
+			LowestGrade = float.MaxValue;
+			
+		}
+		public float AverageGrade;
+		public float HighestGrade;
+		public float LowestGrade;
+	}
+}
